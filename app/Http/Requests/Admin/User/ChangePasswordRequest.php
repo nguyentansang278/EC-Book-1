ab1a95f9177ee95072517eb16e28caff92dd1644
Sang <nguyentansangcv@gmail.com>
2024-11-01 13:59:15 +0700

    refator: Update correct folder name

--- app/Http/Requests/Admin/User/ChangePasswordRequest.php
+++ app/Http/Requests/Admin/User/ChangePasswordRequest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Requests\User;
+namespace App\Http\Requests\Admin\User;
 
 use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
